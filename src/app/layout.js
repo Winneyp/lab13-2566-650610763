25834498d090d7a6eef1cdb17f6437cc1a36155b
P <tatakornaow@gmail.com>
2023-08-07 15:47:10 +0700

    done

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Tayakorn Aowrattanakul"
+            studentId="650610763"
           />
         </div>
       </body>
